1bd186fa2c2dd2119a17065739f531c33daaab4e
26ryss <26ryss@gmail.com>
2024-08-31 20:20:32 +0900

    fix

---
+++ app/lib/placeholder-data.ts
@@ -0,0 +1,22 @@
+const users = [
+  {
+    id: '410544b2-4001-4271-9855-fec4b6a6442a',
+    display_name: 'User',
+    name: 'User',
+    email: 'user@nextmail.com',
+    password: '123456',
+    created_at: '2021-01-01T00:00:00.000Z',
+  },
+];
+
+const stores = [
+  {
+    id: 1,
+    name: 'Lighters',
+    address: '東京都渋谷区神南1-15-7',
+    area_id: 1,
+    description: '渋谷駅徒歩10分に位置する「Lighters」。全席喫煙可能。煙草を吸いながら本格的なイタリアン料理やこだわりの自家製ドリンクを楽しめるお店です。',
+    url: 'https://www.lighters-shibuya.com/',
+    eyeCatch: '/shops/shop.jpg',
+  }
+]
