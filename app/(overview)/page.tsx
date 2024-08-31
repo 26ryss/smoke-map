25548a1de6326f42e92fed1e60168eae8fb09ba4
26ryss <26ryss@gmail.com>
2024-08-31 14:43:03 +0900

    wrap the StoreMapView component with suspense

--- app/(overview)/page.tsx
+++ app/(overview)/page.tsx
@@ -1,10 +1,13 @@
 import StoreMapView from "@/app/ui/top/store-map-view";
+import { Suspense } from 'react';
 
 export default function Home() {
   const totalPages = 3;
   const points = [{lat: 35.662725, lng: 139.700355}];
   
   return (
-    <StoreMapView />
+    <Suspense fallback={<div>Loading...</div>}>
+      <StoreMapView />
+    </Suspense>
   );
 }
