b9c93dc4e3e5faa13694590defc9e6c1ffbeb0d1
26ryss <26ryss@gmail.com>
2024-09-08 09:32:21 +0900

    searchbarのロジック修正

--- app/ui/head/search-bar.tsx
+++ app/ui/head/search-bar.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { TextInput, ActionIcon, Text } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { IconSearch } from '@tabler/icons-react';
-import { colors } from '@/styles/colors';
 import places from '@/app/lib/places';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
@@ -12,12 +11,17 @@ import { useDebouncedCallback } from 'use-debounce';
 export default function SearchBar() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
-  const { replace } = useRouter();
+  const { replace, push } = useRouter();
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
     params.set('area', term);
-    replace(`${pathname}?${params.toString()}`);
+
+    if (pathname !== '/') {
+      push(`/?${params.toString()}`);
+    } else {
+      replace(`${pathname}?${params.toString()}`);
+    }
   }
 
   const handleInputChange = useDebouncedCallback((term)=>{
@@ -42,6 +46,12 @@ export default function SearchBar() {
     }
   });
 
+  useEffect(() => {
+    if (pathname !== '/') {
+      form.setFieldValue('area', '');
+    }
+  }, [pathname])
+
   return (
     <div>
       <Suspense>
@@ -52,10 +62,7 @@ export default function SearchBar() {
             key={form.key('area')}
             styles={{
               input: {
-                borderColor: `${colors.secondary}`, // 任意の色に変更
-                '&:focus': {
-                  borderColor: `${colors.primary}`, // フォーカス時の色も変更
-                },
+                borderColor: "#000",
               },
             }}
             {...form.getInputProps('area')}
@@ -69,7 +76,7 @@ export default function SearchBar() {
             size={36}
             radius="xs"
             aria-label="Gradient action icon"
-            gradient={{ from: `${colors.primary}`, to: `${colors.secondary}`, deg: 90 }}
+            gradient={{ from: "#000", to: "#000", deg: 90 }}
             type="submit"
           >
             <IconSearch style={{ width: '70%', height: '70%' }} stroke={2} />
