d1866a5df63e0cf0e730f9785b2dff465d9a7af3
26ryss <26ryss@gmail.com>
2024-09-12 22:14:11 +0900

    headerをsp対応化

--- app/_components/organisms/projects/header.tsx
+++ app/_components/organisms/projects/header.tsx
@@ -3,6 +3,7 @@ import SearchBar from '@/app/_components/ui-parts/search-bar';
 import Link from 'next/link';
 import { createClient } from '@/utils/supabase/server';
 import { signOutAction } from '@/app/lib/actions';
+import { UserIcon } from '@heroicons/react/24/outline';
 
 export default async function Header() {
   const supabase = createClient();
@@ -13,11 +14,10 @@ export default async function Header() {
   const accountUrl = user ? `/account/${user.id}` : '/login';
 
   return (
-    <header className=" text-white border-b-2 border-slate-100">
-      <nav aria-label="Global" className="flex flex-row items-center px-6 py-3 lg:px-8 justify-between">
+    <header className="text-white border-b-2 border-slate-100 md:w-[1000px] md:mx-auto">
+      <nav aria-label="Global" className="flex flex-row items-center py-1 px-2 md:py-3 justify-between">
         {/* logo */}
         <div className="flex flex-row items-center">
-          <div className="flex lg:flex-1">
             <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Logo of this service</span>
               <Image 
@@ -27,21 +27,38 @@ export default async function Header() {
                 className="hidden md:block"
                 alt="Logo of this service"
               />
+              <Image 
+                src="/logo.png"
+                width={130}
+                height={10}
+                className="md:hidden"
+                alt="Logo of this service"
+              />
             </Link>
-          </div>
-          <div className="ml-10 z-10">
+          <div className="ml-10 z-10 hidden md:block">
             <SearchBar />
           </div>
         </div>
         {!user ? (
-          <div className="flex items-center">
-            <Link href="/signup" className="text-sm font-semibold leading-6 text-gray-900 px-4">
-              会員登録
-            </Link>
-            <Link href="/login" className="text-sm font-semibold leading-6 text-gray-900 px-4">
-              ログイン
-            </Link>
-          </div>
+          <>
+            <div className="items-center flex">
+              <div className="hidden md:flex">
+                <Link href="/signup" className="text-sm font-semibold leading-6 text-gray-900 px-4">
+                  会員登録
+                </Link>
+                <Link href="/login" className="text-sm font-semibold leading-6 text-gray-900 px-4">
+                  ログイン
+                </Link>
+              </div>
+              <div className='md:hidden text-gray-900 text-xs flex flex-col justify-center items-center'>
+                <UserIcon className='w-5'/>
+                <Link href="/login">
+                  ログイン
+                </Link>
+              </div>
+            </div>
+
+          </>
         ) : (
           <div className="flex items-center">
             <Link href={accountUrl} className="text-sm font-semibold leading-6 text-gray-900 px-4">
