6d4bdaa18b3ea082e77f11684d7a3263e74c5347
26ryss <26ryss@gmail.com>
2024-09-15 13:31:48 +0900

    fix

--- app/_components/templates/search/template.tsx
+++ app/_components/templates/search/template.tsx
@@ -17,12 +17,12 @@ export default function Template({
   totalPages: number;
 }) {
   return (
-    <div className="flex py-8">
+    <div className="flex md:py-8">
       <div className="w-3/12 h-[120px] hidden md:block">
         <SideBar area={area}/>
       </div>
       <div className="w-full md:pl-12">
-        <h1 className="text-2xl font-bold text-gray-800 mb-10 bg-gray-50 px-5 py-8">{area}のカフェ</h1>
+        <h1 className="font-semibold px-4 py-1 text-sm bg-gray-100 md:text-2xl md:font-bold text-gray-800 md:mb-10 md:bg-gray-50 md:px-5 md:py-8">{area}のカフェ</h1>
         <div>
           <CardArea stores={stores} reviews={reviews} votes={votes}/>
           <div className="w-full flex justify-center py-4">
